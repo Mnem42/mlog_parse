439bcab54dba192e97bccb6f2a1573e5f1ef2078
Mnem42 <IshaanAbhi25@gmail.com>
2026-01-01 15:35:25 +0000

    Add more test cases

--- src/tests/real_code.rs
+++ src/tests/real_code.rs
@@ -25,3 +25,19 @@ fn base_builder() {
     let lexer = parser::Lexer::new(SRC);
     let _ = lexer.map(|x| x.unwrap()).collect::<Vec<_>>();
 }
+
+#[test]
+fn power_plant() {
+    const SRC: &str = include_str!("../../mlog_files/power_plant.mlog");
+
+    let lexer = parser::Lexer::new(SRC);
+    let _ = lexer.map(|x| x.unwrap()).collect::<Vec<_>>();
+}
+
+#[test]
+fn unit_transport() {
+    const SRC: &str = include_str!("../../mlog_files/unit_transport.mlog");
+
+    let lexer = parser::Lexer::new(SRC);
+    let _ = lexer.map(|x| x.unwrap()).collect::<Vec<_>>();
+}
