3dfa7040db847e8b7e2c3fde79e8ccf2dc9bfb38
Mnem42 <IshaanAbhi25@gmail.com>
2026-01-03 20:13:05 +0000

    feat: better tests for colour parsing

--- src/parser/args/test.rs
+++ src/parser/args/test.rs
@@ -1,4 +1,6 @@
-use crate::parser::args::Rgba;
+use std::str::FromStr;
+
+use crate::parser::args::{Rgba, colour::ColourParseError};
 
 #[test]
 fn colour_literal_unchecked() {
@@ -18,5 +20,39 @@ fn colour_named_literal_unchecked() {
     assert_eq!(
         Rgba::from_named_literal_unchecked("%[foo]"),
         None
+    );
+}
+
+#[test]
+fn colour_fromstr() {
+    assert_eq!(
+        Rgba::from_str("%012345"),
+        Ok(Rgba { r: 0x01, g: 0x23, b: 0x45, a: 0xFF})
+    );
+    assert_eq!(
+        Rgba::from_str("%01234567"),
+        Ok(Rgba { r: 0x01, g: 0x23, b: 0x45, a: 0x67})
+    );
+
+    assert_eq!(
+        Rgba::from_str("%[rED]"),
+        Ok(Rgba { r: 0xE5, g: 0x54, b: 0x54, a: 0xFF })
+    );
+    assert_eq!(
+        Rgba::from_str("%[notacolour]"),
+        Err(ColourParseError::InvalidColourName("notacolour".to_string()))
+    );
+
+    assert_eq!(
+        Rgba::from_str("%01234"),
+        Err(ColourParseError::InvalidColourLiteral("%01234".to_string()))
+    );
+    assert_eq!(
+        Rgba::from_str("%0123456"),
+        Err(ColourParseError::InvalidColourLiteral("%0123456".to_string()))
+    );
+    assert_eq!(
+        Rgba::from_str("%notacolour"),
+        Err(ColourParseError::InvalidColourLiteral("%notacolour".to_string()))
     );
 }
