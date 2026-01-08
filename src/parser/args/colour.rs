0b60a645e09f226d464ac6dddf285ba6cba7b200
Mnem42 <IshaanAbhi25@gmail.com>
2026-01-08 18:34:53 +0000

    clean: fix the mangling rustfmt does to COLOURS

--- src/parser/args/colour.rs
+++ src/parser/args/colour.rs
@@ -9,323 +9,50 @@ use thiserror::Error;
 
 use crate::parser::args::num_parse::parse_hex_arcoid;
 
+#[rustfmt::skip]
 static COLOURS: LazyLock<HashMap<&str, Rgba>> = LazyLock::new(|| {
     HashMap::from([
-        (
-            "white",
-            Rgba {
-                r: 0xFF,
-                g: 0xFF,
-                b: 0xFF,
-                a: 0xFF,
-            },
-        ),
-        (
-            "lightGray",
-            Rgba {
-                r: 0xBF,
-                g: 0xBF,
-                b: 0xBF,
-                a: 0xFF,
-            },
-        ),
-        (
-            "gray",
-            Rgba {
-                r: 0x7F,
-                g: 0x7F,
-                b: 0x7F,
-                a: 0xFF,
-            },
-        ),
-        (
-            "darkGray",
-            Rgba {
-                r: 0x3F,
-                g: 0x3F,
-                b: 0x3F,
-                a: 0xFF,
-            },
-        ),
-        (
-            "black",
-            Rgba {
-                r: 0x00,
-                g: 0x00,
-                b: 0x00,
-                a: 0xFF,
-            },
-        ),
-        (
-            "clear",
-            Rgba {
-                r: 0x00,
-                g: 0x00,
-                b: 0x00,
-                a: 0x00,
-            },
-        ),
-        (
-            "red",
-            Rgba {
-                r: 0xE5,
-                g: 0x54,
-                b: 0x54,
-                a: 0xFF,
-            },
-        ),
-        (
-            "scarlet",
-            Rgba {
-                r: 0xFF,
-                g: 0x34,
-                b: 0x1C,
-                a: 0xFF,
-            },
-        ),
-        (
-            "crimson",
-            Rgba {
-                r: 0xDC,
-                g: 0x14,
-                b: 0x3C,
-                a: 0xFF,
-            },
-        ),
-        (
-            "coral",
-            Rgba {
-                r: 0xFF,
-                g: 0x7F,
-                b: 0x50,
-                a: 0xFF,
-            },
-        ),
-        (
-            "salmon",
-            Rgba {
-                r: 0xFA,
-                g: 0x80,
-                b: 0x72,
-                a: 0xFF,
-            },
-        ),
-        (
-            "pink",
-            Rgba {
-                r: 0xFF,
-                g: 0x69,
-                b: 0xB4,
-                a: 0xFF,
-            },
-        ),
-        (
-            "magenta",
-            Rgba {
-                r: 0xFF,
-                g: 0x00,
-                b: 0xFF,
-                a: 0xFF,
-            },
-        ),
-        (
-            "yellow",
-            Rgba {
-                r: 0xFF,
-                g: 0xFF,
-                b: 0x00,
-                a: 0xFF,
-            },
-        ),
-        (
-            "gold",
-            Rgba {
-                r: 0xFF,
-                g: 0xD7,
-                b: 0x00,
-                a: 0xFF,
-            },
-        ),
-        (
-            "goldenrod",
-            Rgba {
-                r: 0xDA,
-                g: 0xA5,
-                b: 0x20,
-                a: 0xFF,
-            },
-        ),
-        (
-            "orange",
-            Rgba {
-                r: 0xFF,
-                g: 0xA5,
-                b: 0x00,
-                a: 0xFF,
-            },
-        ),
-        (
-            "brown",
-            Rgba {
-                r: 0x8B,
-                g: 0x45,
-                b: 0x13,
-                a: 0xFF,
-            },
-        ),
-        (
-            "tan",
-            Rgba {
-                r: 0xD2,
-                g: 0xB4,
-                b: 0x8C,
-                a: 0xFF,
-            },
-        ),
-        (
-            "brick",
-            Rgba {
-                r: 0xB2,
-                g: 0x22,
-                b: 0x22,
-                a: 0xFF,
-            },
-        ),
-        (
-            "green",
-            Rgba {
-                r: 0x3D,
-                g: 0xD6,
-                b: 0x67,
-                a: 0xFF,
-            },
-        ),
-        (
-            "acid",
-            Rgba {
-                r: 0x7F,
-                g: 0xFF,
-                b: 0x00,
-                a: 0xFF,
-            },
-        ),
-        (
-            "lime",
-            Rgba {
-                r: 0x32,
-                g: 0xCD,
-                b: 0x32,
-                a: 0xFF,
-            },
-        ),
-        (
-            "forest",
-            Rgba {
-                r: 0x22,
-                g: 0x8B,
-                b: 0x22,
-                a: 0xFF,
-            },
-        ),
-        (
-            "olive",
-            Rgba {
-                r: 0x6B,
-                g: 0x8E,
-                b: 0x23,
-                a: 0xFF,
-            },
-        ),
-        (
-            "blue",
-            Rgba {
-                r: 0x00,
-                g: 0x00,
-                b: 0xFF,
-                a: 0xFF,
-            },
-        ),
-        (
-            "navy",
-            Rgba {
-                r: 0x00,
-                g: 0x00,
-                b: 0x7F,
-                a: 0xFF,
-            },
-        ),
-        (
-            "royal",
-            Rgba {
-                r: 0x41,
-                g: 0x69,
-                b: 0xE1,
-                a: 0xFF,
-            },
-        ),
-        (
-            "slate",
-            Rgba {
-                r: 0x70,
-                g: 0x80,
-                b: 0x90,
-                a: 0xFF,
-            },
-        ),
-        (
-            "sky",
-            Rgba {
-                r: 0x87,
-                g: 0xCE,
-                b: 0xEB,
-                a: 0xFF,
-            },
-        ),
-        (
-            "cyan",
-            Rgba {
-                r: 0x00,
-                g: 0xFF,
-                b: 0xFF,
-                a: 0xFF,
-            },
-        ),
-        (
-            "teal",
-            Rgba {
-                r: 0x00,
-                g: 0x7F,
-                b: 0x7F,
-                a: 0xFF,
-            },
-        ),
-        (
-            "purple",
-            Rgba {
-                r: 0x02,
-                g: 0x0F,
-                b: 0x0F,
-                a: 0xFF,
-            },
-        ),
-        (
-            "violet",
-            Rgba {
-                r: 0xEE,
-                g: 0x82,
-                b: 0xEE,
-                a: 0xFF,
-            },
-        ),
-        (
-            "maroon",
-            Rgba {
-                r: 0xB0,
-                g: 0x30,
-                b: 0x60,
-                a: 0xFF,
-            },
-        ),
+        ("white",     Rgba { r: 0xFF, g: 0xFF, b: 0xFF, a: 0xFF }),
+        ("lightGray", Rgba { r: 0xBF, g: 0xBF, b: 0xBF, a: 0xFF }),
+        ("gray",      Rgba { r: 0x7F, g: 0x7F, b: 0x7F, a: 0xFF }),
+        ("darkGray",  Rgba { r: 0x3F, g: 0x3F, b: 0x3F, a: 0xFF }),
+        ("black",     Rgba { r: 0x00, g: 0x00, b: 0x00, a: 0xFF }),
+        ("clear",     Rgba { r: 0x00, g: 0x00, b: 0x00, a: 0x00 }),
+
+        ("red",       Rgba { r: 0xE5, g: 0x54, b: 0x54, a: 0xFF }),
+        ("scarlet",   Rgba { r: 0xFF, g: 0x34, b: 0x1C, a: 0xFF }),
+        ("crimson",   Rgba { r: 0xDC, g: 0x14, b: 0x3C, a: 0xFF }),
+        ("coral",     Rgba { r: 0xFF, g: 0x7F, b: 0x50, a: 0xFF }),
+        ("salmon",    Rgba { r: 0xFA, g: 0x80, b: 0x72, a: 0xFF }),
+        ("pink",      Rgba { r: 0xFF, g: 0x69, b: 0xB4, a: 0xFF }),
+        ("magenta",   Rgba { r: 0xFF, g: 0x00, b: 0xFF, a: 0xFF }),
+
+        ("yellow",    Rgba { r: 0xFF, g: 0xFF, b: 0x00, a: 0xFF }),
+        ("gold",      Rgba { r: 0xFF, g: 0xD7, b: 0x00, a: 0xFF }),
+        ("goldenrod", Rgba { r: 0xDA, g: 0xA5, b: 0x20, a: 0xFF }),
+        ("orange",    Rgba { r: 0xFF, g: 0xA5, b: 0x00, a: 0xFF }),
+
+        ("brown",     Rgba { r: 0x8B, g: 0x45, b: 0x13, a: 0xFF }),
+        ("tan",       Rgba { r: 0xD2, g: 0xB4, b: 0x8C, a: 0xFF }),
+        ("brick",     Rgba { r: 0xB2, g: 0x22, b: 0x22, a: 0xFF }),
+   
+        ("green",     Rgba { r: 0x3D, g: 0xD6, b: 0x67, a: 0xFF }),
+        ("acid",      Rgba { r: 0x7F, g: 0xFF, b: 0x00, a: 0xFF }),
+        ("lime",      Rgba { r: 0x32, g: 0xCD, b: 0x32, a: 0xFF }),
+        ("forest",    Rgba { r: 0x22, g: 0x8B, b: 0x22, a: 0xFF }),
+        ("olive",     Rgba { r: 0x6B, g: 0x8E, b: 0x23, a: 0xFF }),
+   
+        ("blue",      Rgba { r: 0x00, g: 0x00, b: 0xFF, a: 0xFF }),
+        ("navy",      Rgba { r: 0x00, g: 0x00, b: 0x7F, a: 0xFF }),
+        ("royal",     Rgba { r: 0x41, g: 0x69, b: 0xE1, a: 0xFF }),
+        ("slate",     Rgba { r: 0x70, g: 0x80, b: 0x90, a: 0xFF }),
+        ("sky",       Rgba { r: 0x87, g: 0xCE, b: 0xEB, a: 0xFF }),
+        ("cyan",      Rgba { r: 0x00, g: 0xFF, b: 0xFF, a: 0xFF }),
+        ("teal",      Rgba { r: 0x00, g: 0x7F, b: 0x7F, a: 0xFF }),
+   
+        ("purple",    Rgba { r: 0x02, g: 0x0F, b: 0x0F, a: 0xFF }),
+        ("violet",    Rgba { r: 0xEE, g: 0x82, b: 0xEE, a: 0xFF }),
+        ("maroon",    Rgba { r: 0xB0, g: 0x30, b: 0x60, a: 0xFF }),
     ])
 });
 
